a46056269e5e07759ae992723ff34be0f289b987
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-04-23 15:27:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,8 @@ class Triangle
       :isosceles
     elsif side_1 != side_2 || side_2 != side_3
       :scalene
+    elsif side_1 == 0 || side_2 == 0 || side_3 == 0
+      raise
     end
   end
   
